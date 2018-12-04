e8c43024dba1f0a362705b5faf1d28552afaca39
justinvyu <justin.v.yu@gmail.com>
2018-12-03 23:52:11 -0800

    Add day 4

--- 04/04.py
+++ 04/04.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 
 class Guard:
     def __init__(self, guard_id):
         self.guard_id = guard_id
-        self.sleep_times = [] # list of ranges
+        self.sleep_times = np.zeros(60)
         self.awake = True
 
 class Log:
@@ -46,4 +48,35 @@ if __name__ == "__main__":
     logs_data = [convert_log_str(log) for log in logs_txt]
     logs_data.sort(key=lambda log: log.date + log.time)
 
+    guards = {}
+    curr_guard = None
+    sleep_start = 0
+    sleep_end = 0
+    for log in logs_data:
+        if type(log.guard_id) == int:
+            if log.guard_id not in guards:
+                curr_guard = Guard(log.guard_id)
+                guards[log.guard_id] = curr_guard
+            else:
+                curr_guard = guards[log.guard_id]
+        else:
+            if curr_guard.awake:
+                sleep_start = log.minute
+                curr_guard.awake = False
+            else:
+                sleep_end = log.minute
+                for t in range(sleep_start, sleep_end):
+                    curr_guard.sleep_times[t] = curr_guard.sleep_times[t] + 1
+                curr_guard.awake = True
 
+    # -- Part #1: Find sleepiest guard --
+    guard_lst = list(guards.values())
+    guard_lst.sort(key=lambda guard: -sum(guard.sleep_times))
+    sleepiest = guard_lst[0]
+    print(sleepiest.guard_id * np.argmax(sleepiest.sleep_times))
+
+    # -- Part #2: Find most frequent guard (at certain minute) --
+
+    guard_lst.sort(key=lambda guard: -max(guard.sleep_times))
+    most_frequent = guard_lst[0]
+    print(most_frequent.guard_id * np.argmax(most_frequent.sleep_times))
